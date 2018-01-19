2c6918cdb8926dd523bc3e9d533e1cbe86b0a06d
Erik Pearson <eapearson@lbl.gov>
2018-01-18 23:24:06 -0800

    fixes in order to merge PR

--- src/plugin/modules/browser.js
+++ src/plugin/modules/browser.js
@@ -1,67 +1,61 @@
-/*global define*/
-/*jslint white:true,browser:true*/
 define([
+    'uuid',
+    'jquery',
     'kb_service/client/userAndJobState',
     'kb_common/html',
     'plugins/catalog/modules/widgets/kbaseCatalogStats',
-], function(UJS, html, kbaseCatalogStats) {
+], function (
+    Uuid,
+    $,
+    UJS,
+    html,
+    KBaseCatalogStats
+) {
     'use strict';
 
     function factory(config) {
         var runtime = config.runtime,
-            parent, container,
-            jobsClient = new UJS(runtime.getConfig('services.user_job_state.url'), {
-                token: runtime.service('session').getAuthToken()
-            });
-
-        // IMPLEMENTATION
-
-        function render() {
-
-          var t = html.tag,
-              div = t('div');
-
-          var container = div({id : 'container'});
-          return container;
-
-        }
-
+            parent, container, containerId;
+        var jobsClient = new UJS(runtime.getConfig('services.user_job_state.url'), {
+            token: runtime.service('session').getAuthToken()
+        });
 
         // API
 
         function attach(node) {
             parent = node;
             container = parent.appendChild(document.createElement('div'));
+            containerId = new Uuid(4).format();
+            container.id = containerId;
         }
 
         function start(params) {
-          container.innerHTML = render();
+            runtime.send('ui', 'setTitle', 'Jobs Browser');
 
-          // add on a container div, then turn that into a kbaseCatalogStats widget, with a few extra options flagged.
+            // add on a container div, then turn that into a kbaseCatalogStats widget, with a few extra options flagged.
 
-          /* this is...let's charitably say stupid. Certainly obscure.
-             kbaseCatalogStatus isn't exporting anything useful, and when I try to use it as a constructor it fails. I'm not clear
-             why. Maybe the local kbwidget isn't current to the one in narrative?
-
-             I also can't seem to get at $('#container').kbaseCatalogStats() (with or without capital 'B'). Says it's not a function.
-
-             Fortunately, we still have our global KBase registry of all widgets, so we can peel it out of there. For lack of a better
-             idea.
-          */
-          window.KBase.kBaseCatalogStats(
-            {
-              runtime                 : runtime,
-              usernames               : [runtime.service('session').getUsername()],
-              includePublicStats      : false,
-              includeUserRunSummary   : false,
-              useUserRecentRuns       : true,
-            },
-            $('#container')
-          );
-
+            /* this is...let's charitably say stupid. Certainly obscure.
+                kbaseCatalogStatus isn't exporting anything useful, and when I try to use it as a constructor it fails. I'm not clear
+                why. Maybe the local kbwidget isn't current to the one in narrative?
+    
+                I also can't seem to get at $('#container').kbaseCatalogStats() (with or without capital 'B'). Says it's not a function.
+    
+                Fortunately, we still have our global KBase registry of all widgets, so we can peel it out of there. For lack of a better
+                idea.
+            */
+            window.KBase.kBaseCatalogStats(
+                {
+                    runtime: runtime,
+                    usernames: [runtime.service('session').getUsername()],
+                    includePublicStats: false,
+                    includeUserRunSummary: false,
+                    useUserRecentRuns: true,
+                },
+                $('#' + containerId)
+            );
         }
 
-        function stop() {}
+        function stop() { }
 
         function detach() {
             if (container) {
@@ -77,7 +71,7 @@ define([
         };
     }
     return {
-        make: function(config) {
+        make: function (config) {
             return factory(config);
         }
     };
